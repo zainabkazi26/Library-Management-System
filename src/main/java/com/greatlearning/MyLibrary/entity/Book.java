37eb13984a5ac2a889c92d4f687c44e88ea6c120
zainab <zainabkazi26@gmail.com>
2024-10-17 14:30:00 +0530

    initialized

---
+++ src/main/java/com/greatlearning/MyLibrary/entity/Book.java
@@ -0,0 +1,29 @@
+package com.greatlearning.MyLibrary.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Book {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+private String title;
+private String author;
+private String category;
+}
---
+++ src/test/java/com/greatlearning/MyLibrary/MyLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.MyLibrary;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
